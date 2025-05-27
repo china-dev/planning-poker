5f62d031da42c476953a5efb92d786ef5a8b94f2
Lucas Paulino <lucaspaulinodev@gmail.com>
2025-05-27 14:54:42 -0300

    descomenta listener

--- frontend/src/components/room/ListThemes.vue
+++ frontend/src/components/room/ListThemes.vue
@@ -14,12 +14,12 @@ onBeforeMount(() => {
 
 <template>
   <aside class="fixed top-40 left-6">
-    <TransitionGroup name="fade">
+    <!-- <TransitionGroup name="fade">
       <div v-for="item in userStore.themes" :key="item.name" class="mb-4">
         <p class="text-xl font-semibold">📋 {{ item.name }}</p>
         <p v-if="item.mostVoted" class="text-sm font-semibold">🏆 Mais votado: {{ item.mostVoted }}</p>
         <p v-if="item.mostVoted" class="text-sm font-semibold">📊 Média: {{ item.avarage }}</p>
       </div>
-    </TransitionGroup>
+    </TransitionGroup> -->
   </aside>
 </template>
--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -3,7 +3,7 @@
   import VotesRoom from '../components/room/VotesRoom.vue';
   import TableVotes from '../components/room/TableRoom.vue';
   import ListPlayers from '../components/room/ListPlayers.vue';
-  // import ListThemes from '../components/room/ListThemes.vue';
+  import ListThemes from '../components/room/ListThemes.vue';
   import { useUserStore } from '../store/user.ts';
   import { useConnection } from '../composable/useConnection.ts';
   import { onBeforeUnmount } from "vue";
@@ -14,15 +14,13 @@
   
   onBeforeUnmount(() => {
     removeListeners();
-    console.log('teste');
-    
   });
 </script>
 
 <template>
   <main class="flex flex-col justify-between h-full">
     <ListPlayers />
-    <!-- <ListThemes /> -->
+    <ListThemes />
     <HeaderRoom />
     <TableVotes />
     <VotesRoom

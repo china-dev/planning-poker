183c5001aa54f779da9773b91986d0c4d6485052
Lucas Paulino <lucaspaulinodev@gmail.com>
2025-05-27 14:55:58 -0300

    erros fix

--- frontend/src/components/room/ListThemes.vue
+++ frontend/src/components/room/ListThemes.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { useUserStore } from '../../store/user';
+// import { useUserStore } from '../../store/user';
 import { onBeforeMount } from 'vue';
 import { useVotes } from '../../composable/useVotes';
 
-const userStore = useUserStore();
+// const userStore = useUserStore();
 const { startLinstenerOnInitVotes } = useVotes();
 
 onBeforeMount(() => {
